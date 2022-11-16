5566e922a8002dbeacf19853e511fbbee7086ea0
Nahomi Carmona <nahomi.carmona@maxiconsumo.com>
2022-11-15 21:45:51 -0300

    feat(learning): adding logic to bring list of users with avatars

--- src/App.js
+++ src/App.js
@@ -1,12 +1,67 @@
-import React from 'react';
+import React,{useState } from "react";
 import { Button } from './components/Button/Button';
 
-function App() {
-  return (
-    <div className="App">
-     <Button onClick={onclick} text='Get users' />
-    </div>
-  );
-}
+export default function App() {
+  const [users, setUsers] = useState();
+  const [setError] = useState();
 
-export default App;
+  
+
+  const handleClick = async () => {
+   
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      if (response.ok) {
+        const data = await response.json();
+        
+        getAllAvatar(data);
+      } else {
+          setError("there was an error getting the user");
+      }
+    } catch (error) {
+        setError("We couldn't make the request to get the user");
+    }
+  }
+
+    const getAllAvatar = async (data) => {
+      let newUsersList = [];
+      try {
+        for (let i = 0; i < data.length; i++) {
+          const response = await fetch(
+            "https://avatars.dicebear.com/api/male/john.svg?background=%230000ff"
+          );
+          const avatar = await response.text();
+          newUsersList.push({ ...data[i], avatar });
+        }
+
+        setUsers(newUsersList);
+      } catch (error) {
+        setError("We couldn't make the request to get the user");
+      }
+    }
+ 
+    return (
+      <div className='App'>
+        <Button onClick={handleClick} text='Users' />
+  
+        {users?.length === 0 ? (
+          <div>Users available</div>
+        ) : (
+          <div>
+            {users?.map((user) => (
+              <div>
+                <div
+                  dangerouslySetInnerHTML={{ __html: user.avatar }}
+                  style={{ width: 100, height: 100 }}
+                />
+                <div>{user.name}</div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  }
+ 
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import './Button.css';
 
 export const Button = (props) => {
   return (
-    <button className="button" type="button" onClick={props.onClick}>
-    </button>
+    <button className="button" type="button" onClick={props.onClick}>{props.text}</button>
   );
 };
 
